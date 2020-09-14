7152a741d337915c644fe06caba18a953a58b336
Jéssica Pimentel <jessikpimentel@hotmail.com>
2020-09-13 22:33:08 -0300

    git init
    Signed-off-by: Jéssica Pimentel <jessikpimentel@hotmail.com>

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('**', (req, res) => {
+    res.send('Error');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
